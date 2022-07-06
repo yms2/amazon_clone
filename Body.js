e01d48e0fa68f53bfb47573b98bc60094455f6d9
Yu Min Seung <90673332+yms2@users.noreply.github.com>
2022-07-06 12:34:37 +0900

    Add files via upload

---
+++ Body.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Body.css'
+
+function Body(){
+    return(
+        <div className='Body'>
+            <div className='Body_left'>
+
+                <div className='li_1'>
+                <h3>Department</h3>
+                    <span><a href=''>＜ Make up </a></span><br/>
+                    <span><a href=''>＜ Skin Care </a></span><br/>
+                    <span><a href=''>＜ Hair Care </a></span><br/>
+                    <span><a href=''>＜ Fragrance </a></span>
+                    <span><a href=''>＜ Foot,Hand & Nail Care </a></span>
+                    <span><a href=''>＜ Tools & Accessories</a></span>
+                    <span><a href=''>＜ Shave & Hair Removal</a></span>
+                    <span><a href=''>＜ Personal Care</a></span>
+                    <span><a href=''>＜ Salon & Spa Equipment</a></span>
+                </div>
+
+                <div className='li_2'>
+                <h3>Premium Beauty</h3>
+                <p><input type={'checkbox'}></input>Premium Selection</p>
+                </div>
+                <div className='li_3'>
+                    
+                </div>
+            </div>
+            <div className='Body_top'></div>
+            <div className='Body_right'>
+                <div className='items'>
+                    <ul>
+                        <li className='box1'>
+                            <h2>Gaming accessories</h2>
+                        </li>
+                        <li className='box1'>
+                            <h2>Shop by Category</h2>
+                        </li>
+                        <li className='box1'>
+                            <h2>Electronics</h2>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Body;
